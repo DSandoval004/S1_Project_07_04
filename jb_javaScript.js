ebeb2ef6dc025863590eebb703ab70f5ce2eaae6
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-04 15:24:57 -0700

    Commit Push 12/4/18
    Fixed and finished top nav bar. Then started the article section, put in images of surfer surfing, and put the content in boxes and made them in flex boxes. Added some responsive design.

--- jb_javaScript.js
+++ jb_javaScript.js
@@ -1,23 +1,11 @@
 // Hover Effect
-// home
-function navHome() {
-    document.getElementById('navHome').style.color = 'white'
-    document.getElementById('NavHome').style.backgroundColor = 'blue'
-}
-
-function NavHome() {
-    document.getElementById('navHome').style.color = 'black'
-    document.getElementById('NavHome').style.backgroundColor = 'transparent'
-}
 // classes
 function navClasses() {
-    document.getElementById('navClasses').style.color = 'white'
-    document.getElementById('NavClasses').style.backgroundColor = 'blue'
+    document.getElementById('NavHome').style.display = 'block'
 }
 
 function NavClasses() {
-    document.getElementById('navClasses').style.color = 'black'
-    document.getElementById('NavClasses').style.backgroundColor = 'transparent'
+    document.getElementById('navHome').style.display = 'none'
 }
 // camps
 function navCamps() {
@@ -27,37 +15,6 @@ function navCamps() {
 
 function NavCamps() {
     document.getElementById('navCamps').style.color = 'black'
-    document.getElementById('NavCamps').style.backgroundColor = 'transparent'
-}
-// store
-function navStore() {
-    document.getElementById('navStore').style.color = 'white'
-    document.getElementById('NavStore').style.backgroundColor = 'blue'
-}
-
-function NavStore() {
-    document.getElementById('navStore').style.color = 'black'
-    document.getElementById('NavStore').style.backgroundColor = 'transparent'
-}
-// blog
-function navBlog() {
-    document.getElementById('navBlog').style.color = 'white'
-    document.getElementById('NavBlog').style.backgroundColor = 'blue'
-}
-
-function NavBlog() {
-    document.getElementById('navBlog').style.color = 'black'
-    document.getElementById('NavBlog').style.backgroundColor = 'transparent'
-}
-// staff
-function navStaff() {
-    document.getElementById('navStaff').style.color = 'white'
-    document.getElementById('NavStaff').style.backgroundColor = 'blue'
-}
-
-function NavStaff() {
-    document.getElementById('navStaff').style.color = 'black'
-    document.getElementById('NavStaff').style.backgroundColor = 'transparent'
 }
 // PullDown
 function pullDownClasses() {
