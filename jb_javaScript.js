058b07746f58256ad83dfa670bd9654ef9dc2860
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-05 15:22:42 -0700

    Finished Code need comments
    Made it have mobile response.  Put in a print styles and html for print. put in a footer with an hr in it. Changed the JavaScript and made it put at at certain spot when load on mobile. Refined styles. Made the section a flex-container and a flex-box. Made the description list a box and made them look fancy and responsive. Then put in responsive design.

--- jb_javaScript.js
+++ jb_javaScript.js
@@ -1,26 +1,4 @@
-// Hover Effect
-// classes
-function navClasses() {
-    document.getElementById('NavHome').style.display = 'block'
-}
-
-function NavClasses() {
-    document.getElementById('navHome').style.display = 'none'
-}
-// camps
-function navCamps() {
-    document.getElementById('navCamps').style.color = 'white'
-    document.getElementById('NavCamps').style.backgroundColor = 'blue'
-}
-
-function NavCamps() {
-    document.getElementById('navCamps').style.color = 'black'
-}
-// PullDown
-function pullDownClasses() {
-    document.getElementById('classes').style.display = 'block'
-}
-
-function pullUpClasses() {
-    document.getElementById('classes').style.display = 'none'
+// 
+if (screen.width <= 480) {
+    document.location.url = "index.html#mobileTop";
 }
