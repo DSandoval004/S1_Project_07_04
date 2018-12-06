cbf7382b1b2166c842bcda1c09a3bab66c7e9cd7
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-06 15:05:56 -0700

    Finished Code Need Comments
    Added flex-box and flex item to basic. Added a Nav Icon to hide and display the nav-bar when it is on mobile.

--- jb_javaScript.js
+++ jb_javaScript.js
@@ -2,3 +2,15 @@
 if (screen.width <= 480) {
     document.location.url = "index.html#mobileTop";
 }
+// 
+function navShow() {
+    document.getElementById('navIcon').style.display = 'none'
+    document.getElementById('navX').style.display = 'block'
+    document.getElementById('headerRight').style.display = 'block'
+}
+
+function navHide() {
+    document.getElementById('navIcon').style.display = 'block'
+    document.getElementById('navX').style.display = 'none'
+    document.getElementById('headerRight').style.display = 'none'
+}
