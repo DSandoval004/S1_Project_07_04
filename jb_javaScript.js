fffe20eb26aeaddbc775df7ba2da18a4a5471726
Diego Sandoval <dsandoval004@west-mec.org>
2018-12-07 15:30:12 -0700

    11/7/18 commit push
    Added a lot more comments, but it still needs more comments.

--- jb_javaScript.js
+++ jb_javaScript.js
@@ -1,8 +1,8 @@
-// 
+// Makes it so when the page is launched on mobile, it starts in the right spot.
 if (screen.width <= 480) {
     document.location.url = "index.html#mobileTop";
 }
-// 
+// Allows a button that hides and shows the navgation when in mobile.
 function navShow() {
     document.getElementById('navIcon').style.display = 'none'
     document.getElementById('navX').style.display = 'block'
